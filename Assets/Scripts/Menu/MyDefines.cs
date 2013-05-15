96cd49ecf8c7660ef9f52a1a7393e8e2d6f1770a
Romain Dubuc <romain.dubuc@gmail.com>
2013-05-16 01:13:44 +0200

    Add Level 5

--- Assets/Scripts/Menu/MyDefines.cs
+++ Assets/Scripts/Menu/MyDefines.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public static class MyDefines 
 {
 	public static int kNbWorlds          = 4;
-	public static int kLevelsByWorld     = 4;
+	public static int kLevelsByWorld     = 5;
 	public static int kNbLevels          = kNbWorlds * kLevelsByWorld;
 	public static int kNbScrewsByLevel   = 3;
 	public static int kNbScrews          = kNbScrewsByLevel * kNbLevels;
