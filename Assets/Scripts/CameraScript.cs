3ccb97fcd94bc293f8484c421cdfbf2bac2d1fb8
michel.topuzoglu <michel.topuzoglu@gmail.com>
2013-06-06 23:20:12 +0200

    Old camera put back + new build
    Signed-off-by: michel.topuzoglu <michel.topuzoglu@gmail.com>

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -16,20 +16,12 @@ public class CameraScript : MonoBehaviour
 		smoothTime = GlobalVarScript.instance.cameraSmoothDefault;
 		velocity = Vector3.zero;
 		lastPos = Vector3.zero;
-		limitYMax = 0.8f;
-		limitYMin = 0.2f;
+		limitYMax = 0.9f;
+		limitYMin = 0.35f;
 		doDecal = 0;
 	}
 	
-	void FixedUpdate()
-	{
-
-			UpdateCamera ();
-		
-	}
-
-	
-	void UpdateCamera ()
+	void FixedUpdate ()
 	{
 		smoothTime = GlobalVarScript.instance.cameraSmooth;
 		Vector3 point = camera.WorldToViewportPoint(GlobalVarScript.instance.cameraTarget.position);
@@ -46,55 +38,41 @@ public class CameraScript : MonoBehaviour
 		if (GlobalVarScript.instance.cameraTarget.tag == "Target")
 		{
 			// augmente la vitesse de smooth quand le perso est proche du bord de l'ecran
-			if (lastPos.y - GlobalVarScript.instance.cameraTarget.position.y > 0.001f)
+			if (lastPos.y > GlobalVarScript.instance.cameraTarget.position.y)
 			{
 				// cible en train de tomber
-				if (point.y >= limitYMax)
+				if (doDecal == 2)
 				{
 					float posDecal = lastPos.y - GlobalVarScript.instance.cameraTarget.position.y;
-					Vector3 newPos = new Vector3(smoothPos.x, transform.position.y - posDecal, smoothPos.z);
+					float camDecal = posDecal * 1.5f;
+					Vector3 newPos = new Vector3(smoothPos.x, transform.position.y - (point.y < limitYMax ? camDecal : posDecal), smoothPos.z);
 					transform.position = newPos;
 					GlobalVarScript.instance.cameraFixedPos = Vector3.zero;
 				}
-				else if (doDecal == 2 && point.y < limitYMax && GlobalVarScript.instance.cameraFixedPos == Vector3.zero)
+				else if (doDecal == 1)
 				{
-					float posDecal = lastPos.y - GlobalVarScript.instance.cameraTarget.position.y;
-					float camDecal = posDecal * 1.4f;
-					Vector3 newPos = new Vector3(smoothPos.x, transform.position.y - camDecal, smoothPos.z);
-					transform.position = newPos;
+					transform.position = smoothPos;
 				}
 				else
 				{
 					transform.position = smoothPos;
 				}
-				doDecal = 2;
 			}
-			else if (lastPos.y - GlobalVarScript.instance.cameraTarget.position.y < -0.001f)
+			else if (point.y > limitYMax && GlobalVarScript.instance.cameraTarget.position.y > lastPos.y)
 			{
-				if (point.y >= limitYMax && GlobalVarScript.instance.cameraFixedPos == Vector3.zero)
-				{
-					float posDecal = lastPos.y - GlobalVarScript.instance.cameraTarget.position.y;
-					Vector3 newPos = new Vector3(smoothPos.x, transform.position.y - posDecal, smoothPos.z);
-					transform.position = newPos;
-				}
-				else
-				{
-					transform.position = smoothPos;
-				}
+				// cible atteint haut ecran
+				Vector3 newPos = new Vector3(smoothPos.x, transform.position.y + GlobalVarScript.instance.cameraTarget.position.y - lastPos.y, smoothPos.z);
+				transform.position = newPos;
+				GlobalVarScript.instance.cameraFixedPos = Vector3.zero;
 			}
 			else
 			{
 				transform.position = smoothPos;
 			}
-			/*
+			
 			if (doDecal == 0 && point.y < limitYMin && lastPos.y > GlobalVarScript.instance.cameraTarget.position.y)
 			{
 				doDecal = 2;
-			}
-			*/
-			if (GlobalVarScript.instance.cameraFixedPos != Vector3.zero && (point.y >= limitYMax || point.y <= limitYMin))
-			{
-				GlobalVarScript.instance.cameraFixedPos = Vector3.zero;
 			}
 		}
 		else if (c == null)
@@ -113,7 +91,6 @@ public class CameraScript : MonoBehaviour
 	public void Reset()
 	{
 		this.doDecal = 0;
-		lastPos = GlobalVarScript.instance.cameraTarget.position;
 	}
 	
 	private static IEnumerator MoveOverTime(Transform theTransform, Vector3 d, float t)
@@ -123,7 +100,7 @@ public class CameraScript : MonoBehaviour
 	
 		Vector3 startPosition = theTransform.position;
 		Vector3 endPosition = d;
-		endPosition.z = -GlobalVarScript.instance.cameraZOffset;
+		endPosition.z = -10f;
 	
 		while( index < 1.0 )
 		{
